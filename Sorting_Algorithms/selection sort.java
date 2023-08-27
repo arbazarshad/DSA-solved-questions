ea503d01135c2aba2957798e1c4cdf88d83cda5d
Mohammad Arbaz <95042697+arbazarshad@users.noreply.github.com>
2023-08-27 16:07:07 +0530

    Update selection sort.java

--- Sorting_Algorithms/selection sort.java
+++ Sorting_Algorithms/selection sort.java
@@ -50,3 +50,38 @@ public static void main(String[] args) {
 
 
 Method II: using Recursion
+Code:
+
+    public static void main(String[] args) {
+        int[] arr = {1,7,5,4,8,3,2,6,9};
+        System.out.println(Arrays.toString(selectionsort(arr,0,arr.length)));
+        
+    }
+    private static int[] selectionsort(int[] arr,int start,int end) {
+        if(start == end-1)
+        {
+            return arr;
+        }
+        
+        int minindex = dinfindex(arr,start);
+        if(minindex == start)
+        {
+            return selectionsort(arr, start+1, end);
+        }
+        int temp = arr[start];
+        arr[start] = arr[minindex];
+        arr[minindex] = temp;
+        return selectionsort(arr, start+1, end);
+    }
+
+    private static int dinfindex(int[] arr, int start) {
+        int index = start;
+        for(int i = start+1;i<arr.length;i++)
+        {
+            if(arr[index]>arr[i])
+            {
+                index = i;
+            }
+        }
+        return index;
+    }
